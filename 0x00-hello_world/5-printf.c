0593d79db93e1943615fe1bf1c41b8f9653c11c8
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-15 04:09:33 -0700

    Printf it

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}

59e19f23d809fa40abaae08fb270c37739bfa05d
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-17 07:38:16 -0700

    Checking for the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,11 +25,12 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	else
+	else if (ld == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
 
 
 	return (0);
+}
 

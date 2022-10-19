09401259eabbbda68151fc8771afa67c7714b8c8
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-19 03:33:11 -0700

    put it

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * main - Entry Point
+ *
+ * pritn '_putchar' followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a[] = "_putchar";
+	int len = strlen(a); /* set length of str 'a' in a variable */
+	int i = 0; 
+	
+	while (i < len)
+	{
+		_putchar(a[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}

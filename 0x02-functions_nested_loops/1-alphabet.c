48c4434765a0e52469eb1afe555c6c729706ed5f
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-19 04:35:30 -0700

    void functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet -prints all lowercase alphabets
+ * Return: void
  */
 
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 void print_alphabet(void)
 {

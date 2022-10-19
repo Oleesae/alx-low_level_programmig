090dda98e1070659afe9327a1fe4d01d453a0024
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-19 08:38:27 -0700

    fetch absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Entry
+ *
+ * @c: absolute value is returned
+ *
+ * Return: return the result of abs
+ */
+
+int _abs(int c)
+{
+	return (abs(c));
+}

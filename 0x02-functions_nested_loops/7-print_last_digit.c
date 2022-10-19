6057765d8babcdbdd682258732856ee5c3dabd4b
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-19 08:50:24 -0700

    fetch the last digit again

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_last_digit - Entry, inputs the value to be evaluated
+ *
+ * @d: number to be checkes
+ *
+ * Return: the last digit of d
+ */
+
+int print_last_digit(int d)
+{
+	int ld = d % 10;
+
+	return (ld);
+}

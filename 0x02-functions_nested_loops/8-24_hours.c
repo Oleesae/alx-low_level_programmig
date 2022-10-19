166d57ef5c111c3c4b1f382571f19cff1401c51b
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-19 09:30:13 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ void jack_bauer(void)
 	for (min = 0; min < (60 * 24); min++)
 	{
 		hour = min / 60;
-		min = min % 60;
+		m = min % 60;
 		_putchar(48 + (hour / 10));
 		_putchar(48 + (hour % 10));
 		_putchar(':');

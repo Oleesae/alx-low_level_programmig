ce97380f8c726e83ca71639dbe8c3605cef6b31c
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-20 03:43:29 -0700

    Add up

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * add - prints the sum of two integers
+ *
+ * @a: integer to be summed
+ * @b: integer to be summed
+ *
+ * Return: returns 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int s = a + b;
+
+	_putchar('0' + s);
+
+	return (0);
+}

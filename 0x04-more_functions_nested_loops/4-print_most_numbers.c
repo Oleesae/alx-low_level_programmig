e5213dc60b1e607462ea2a2458bb2b0469029c14
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-27 14:40:19 -0700

    Except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,20 @@
 
 void print_most_numbers(void)
 {
-	int a;
+	#define START 48
+	#define END 57
+	#define TWO 50
+	#define FOUR 52
+	int a = START;
 
-	for (a = '0'; a <= '9'; a++)
+	while (a <= END)
 	{
-		if (a != '2' || a != '4')
+		if (a != TWO || a != FOUR)
 		{
 			_putchar(a);
+			a++;
 		}
+		a++;
 	}
 	_putchar('\n');
 }

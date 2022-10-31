6c4c402fd624484b503b934ae6947ae79586ab95
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-10-31 11:56:26 -0700

    98 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long int f1 = 1, f2 = 2, f3, i; /* 1st and 2nd fibonacci numbers */
+
+	#define LIM 98
+	printf("%lu, ", f1); /* print the first number */
+
+	/* iterate to print through the rest */
+	for (i = 1; i < LIM; i++)
+	{
+		printf("%lu", f2);
+
+		/* print a comma and space if not the last number */
+		if (i != (LIM - 1))
+			printf(", ");
+
+		f3 = f2 + f1;
+		f1 = f2;
+		f2 = f3;
+	}
+	printf("\n");
+	return (0);
+}

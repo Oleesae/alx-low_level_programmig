1e8c71139984c663d7e7facaa5ec6929aa0200cc
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-05 10:11:41 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = _strlen(*s);
+
+	while (len >= 0)
+	{
+		_putchar(*s);
+		len--;
+	}
+	_putchar('\n');
+}

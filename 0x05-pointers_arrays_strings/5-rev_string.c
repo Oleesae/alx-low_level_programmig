1e527018df0e8304ebfd570678cb971174a3a94a
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-07 14:07:29 -0800

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,37 @@
 #include "main.h"
 
 /**
- * rev_string - prints a string in reverse
+ * rev_string - reverses a string
  * @s: string
  * Return: void
  */
 
 void rev_string(char *s)
 {
-	int ln; /* length counter */
+	int len; /* length */
+	char c; /* temp character holder */
+	int lind; /* index from the left/beginning */
+	int rind; /* index from the right/end */
 
-	/* get the length of *s */
-	for (ln = 0; s[ln] != '\0'; ln++)
+	/* counts each char value in the string until NULL */
+	for (len = 0; s[len] != '\0'; len++)
 		continue; /* do nothing */
-	/* ln equals index of \0 */
-	ln -= 1;
-	/* ln equals last index */
-	while (ln >= 0)
+
+	 lind = 0; /* first index of string */
+	 rind = len - 1; /* last index of string */
+
+	/**
+	 * rind always greater than lind
+	 * save the value of lind in temp holder,
+	 * swap the two ends till they meet at the middle
+	 */
+	while (rind >= lind)
 	{
-		_putchar(s[ln]);
-		ln--;
+		c = s[lind];
+		s[lind] = s[rind];
+		s[rind] = c;
+
+		lind++;
+		rind--;
 	}
-	_putchar('\n');
 }

4c31bf9f669cb94abd412dd621fe5d2910532409
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-07 14:44:01 -0800

    Print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the last half of a string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int n, i;
+
+	/* length of string */
+	for (i = 0; str[i] != '\0'; i++)
+		continue;
+
+	/* get half of string */
+	if ((i % 2) != 0) /* if length of string is odd */
+		n = (i - 1) / 2;
+	else
+		n = i / 2;
+
+	/* print from middle of string to the end */
+	while (n < i) 
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');

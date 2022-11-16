5dea29c9e9e5cf96c16366c9be96dabe3c26b887
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-16 02:10:34 -0800

    Convert lower to upper in string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lowercase to uppercase in a string
+ * @str: string
+ *
+ * Return: character
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		/* check if lowercase */
+		if (str[i] > 96 && str[i] < 123) 
+		{
+			str[i] -= 32; /* change to uppercase */
+		}
+		i++;
+	}
+	return (str);
+}

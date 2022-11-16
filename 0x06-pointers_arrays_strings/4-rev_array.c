b6c4833a9dd26206f94534d46c48becbd1e4bd6b
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-15 23:43:26 -0800

    Reverse string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: array
+ * @n: number of elements of the array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp_ind, i, j;
+
+	i = 0; /* first index */
+	j = n - 1; /* last index */
+	while (i < j)
+	{
+		tmp_ind = a[i]; /* store first index */
+		a[j] = tmp_ind; /* transfer to last index */
+		i++;
+		j--;
+	}
+}

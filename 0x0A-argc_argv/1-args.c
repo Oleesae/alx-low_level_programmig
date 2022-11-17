0e611cc36902f8c60c239924e8139adefaacbcb9
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-17 15:24:33 -0800

    Whats my name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (*argv)
+		printf("%d\n", argc);
+	return (0);
+}

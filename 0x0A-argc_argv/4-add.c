918cfedc3bf1902c7f5a01db62f89e98814694df
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-23 07:44:02 -0800

    changed the condition of the first for loop in 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,20 +9,22 @@
  * Return: Always 0 or 1
  */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	unsigned int val;
 	int i, c;
 
-	for (i = 1; argv[i]; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (c = 0; argv[i][c]; c++)
+		{
 			if (!isdigit(argv[i][c]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-		val += atoi(argv[i]);
+			val += atoi(argv[i]);
+		}
 	}
 	printf("%d\n", val);
 	return (0);

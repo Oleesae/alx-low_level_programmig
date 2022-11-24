bec02f34ad81dd5aab9b0a1b9593b92c942d0732
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-24 11:04:01 -0800

    sum pi object

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 0_OBJECT_LIKE_MACRO_H
-#define 0_OBJECT_LIKE_MACRO_H
+#ifndef __0_OBJECT_LIKE_MACRO_H__
+#define __0_OBJECT_LIKE_MACRO_H__
 
 #define SIZE 1024
 
-#endif /* 0_OBJECT_LIKE_MACRO_H */
+#endif /* __0_OBJECT_LIKE_MACRO_H__ */

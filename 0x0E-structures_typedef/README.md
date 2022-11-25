0fa5bb986d5207ec8292d75a662cc38a8bcc9a19
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-25 08:50:01 -0800

    Completed Task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+0x0E-structures\_typedef is a project on
+-C structures and
+-typedef
+
+What are structures, when, why and how to use them
+How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - a dog struct
+ * @name: name of dog, char *
+ * @age: age of dog, float
+ * @owner: owner of dog, char *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* __DOG_H__ */

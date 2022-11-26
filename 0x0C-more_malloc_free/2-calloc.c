39063fd76122bce959b82e684afda26aaaab26e6
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-26 05:40:01 -0800

    create _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates nmemb of elements of array size bytes each
+ * @nmemb: number of elements to allocate memory
+ * @size: size of each elements
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	/* exit if a null byte is passed */
+	if (nmemb < 1 || size < 1)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	/* exit if ptr returns null */
+	if (ptr == NULL)
+		return (NULL);
+	/* assign 0 to each byte of ptr */
+	while (ptr[i])
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+	free(ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,11 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 #include <stdlib.h>
+#include <stdio.h>
 
 void _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /* __MAIN_H__ */

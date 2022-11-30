324539429053fcc771e69860838b0a81a7e02413
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-11-30 08:23:07 -0800

    create print_numbers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
-void print_number(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* __VARIADIC_FUNCTIONS_H__ */

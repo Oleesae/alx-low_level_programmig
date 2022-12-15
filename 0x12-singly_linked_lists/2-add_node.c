6d0eb7198a8925398b72044af3fee709b396d0d5
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-12-15 05:37:14 -0800

    corrected free

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -30,7 +30,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return (NULL):
+		return (NULL);
 	}
 	new_node->len = _len(new_node->str);
 	new_node->next = *head;

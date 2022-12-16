25d8ae85aafdd84098aa9c1346dec84640e8d8e2
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-12-16 05:49:00 -0800

    free to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,12 +6,12 @@
  * Return: void
  */
 
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
-	if (head == NULL)
+	if (*head == NULL)
 		return;
 	/* use recursion to access all nodes and free allocated memory */
-	free_listint2(head->next);
-	free(head);
-	head = NULL;
+	free_listint2(*head->next);
+	free(*head);
+	*head = NULL;
 }

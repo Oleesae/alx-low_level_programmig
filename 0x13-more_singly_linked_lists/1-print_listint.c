49b021a5daceceb935ae1cc6cc2b57dee9962aec
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-12-16 04:39:33 -0800

    complete

---
+++ 0x13-more_singly_linked_lists/1-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the elements of a list_t list
+ * @h: pointer to the struct list_t
+ * Return: size_t
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodeTotal = 0;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodeTotal += 1;
+	}
+	return (nodeTotal);
+}

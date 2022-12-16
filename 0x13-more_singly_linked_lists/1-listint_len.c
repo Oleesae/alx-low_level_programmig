004a780f536340433b65b609fb089ed0b8737c8e
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-12-16 04:46:06 -0800

    complete

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,16 @@
 #include "lists.h"
 
 /**
- * print_listint - prints the elements of a list_t list
+ * listint_len - returns the number elements of a list_t list
  * @h: pointer to the struct list_t
  * Return: size_t
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t nodeTotal = 0;
 	
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		nodeTotal += 1;
 	}

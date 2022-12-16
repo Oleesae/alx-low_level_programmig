a5e2cd2a4923aa3ab645d423699f2e480900963b
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-12-16 06:57:32 -0800

    free 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a malloced list
+ * free_listint2 - frees a malloced list
  * @head: pointer to head of list
  * Return: void
  */
 
 void free_listint2(listint_t **head)
 {
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return;
 	/* use recursion to access all nodes and free allocated memory */
-	free_listint2((*head)->next);
+	free_listint2(&((*head)->next));
 	free(*head);
 	*head = NULL;
 }

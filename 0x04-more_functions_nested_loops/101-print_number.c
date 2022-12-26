6af04ed2fb9ca3dafd2a3eebbe9ff62398277d14
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-12-26 02:05:01 +0100

    prints a number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+}

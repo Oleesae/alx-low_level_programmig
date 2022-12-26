2c4f803dd9d027197927a2503610da1a963cb538
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2022-12-26 02:52:36 +0100

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: an array of char
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8]) {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			_putchar(a[i][j]);
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}

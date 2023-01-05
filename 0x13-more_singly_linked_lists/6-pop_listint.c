8219bac26956709b236c5a82fa26afba7f693979
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-05 14:21:01 +0100

    completed task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head of a list
+ * @head: head of the list
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return (0);
+
+	temp = *head;
+	*head = *head->next;
+	free(temp);
+	return (*head->n);
+}

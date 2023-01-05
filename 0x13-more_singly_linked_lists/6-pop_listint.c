ce89dd2480c2749b214300f451377a8b64749a44
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-05 14:30:36 +0100

    fix: change return value

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int n;
 
 	if (head == NULL)
 		return (0);
-
+	n = (*head)->n
 	temp = *head;
 	*head = (*head)->next;
 	free(temp);
-	return ((*head)->n);
+	return (n);
 }

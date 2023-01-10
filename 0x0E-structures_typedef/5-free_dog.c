cc63552bc19b9db6e6c166bb76518531a8411293
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-10 10:22:33 +0100

    completed: freed dog_t

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog_t struct
+ * @d: pointer to a dog_t struct
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		free(d);
+	free(d->name);
+	free(d->owner);
+	free(d);
+}

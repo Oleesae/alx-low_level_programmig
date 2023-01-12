bd287c1f49adbcbbb05be98df4cc36cca728eef8
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-12 20:15:21 +0100

    completed: number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements of a dlistint_t
+ * @h: pointer to head of dlistint_t
+ *
+ * Return: size_t (number of nodes)
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t total_nodes = 0;
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+		total_nodes++;
+	}
+	return (total_nodes);
+}

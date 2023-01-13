769e5bb9878c96a95c840bd9e61adeb1d0560196
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-13 19:47:48 +0100

    add: conditions for start and end of node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - returns the number of elements of a dlistint_t
+ * @h: pointer to head of dlistint_t
+ *
+ * Return: size_t (number of nodes)
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t total_nodes = 0;
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+		total_nodes++;
+	}
+	return (total_nodes);
+}
+
+/**
  * insert_dnodeint_at_index - inserts the element of a dlistint_t at index
  * @h: pointer to head of dlistint_t
  * @idx: index to insert
@@ -10,7 +30,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
+	unsigned int i, len;
 	dlistint_t *new;
 	dlistint_t *bef, *aft;
 
@@ -19,22 +39,34 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		while ((*h)->prev)
 			(*h) = (*h)->prev;
 	}
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	len = dlistint_len(*h);
+	if (idx == 0)
 	{
-		free(new);
-		return (NULL);
+		new = add_dnodeint(h, n);
 	}
-	new->n = n;
-	bef = *h;
-	for (i = 0; i < idx - 1; i++)
+	else if (idx == len - 1)
 	{
-		bef = bef->next;
+		new = add_dnodeint_end(h, n);
 	}
-	aft = bef->next;
-	new->next = aft;
-	aft->prev = new;
-	bef->next = new;
-	new->prev = bef;
+	else
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (!new)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->n = n;
+		bef = *h;
+		for (i = 0; i < idx - 1; i++)
+		{
+			bef = bef->next;
+		}
+		aft = bef->next;
+		new->next = aft;
+		aft->prev = new;
+		bef->next = new;
+		new->prev = bef;
+	}
 	return (new);
 }

cf93c101ed0ae15a7dee18b39381aa5c0025c54d
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-13 16:53:39 +0100

    completed: freed up dlistint_t

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: head of dlistint_t
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	if (head)
+	{
+		while (head->prev)
+		{
+			head = head->prev;
+		}
+	}
+	
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}

1e3d6fc623b8e766d09f8f062b67cb9b83184eb4
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-31 22:23:12 +0100

    complete: test delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,73 +1,45 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements of a dlistint_t
- * @h: pointer to head of dlistint_t
+ * delete_dnodeint_at_index - deletes the node at index index
+ * of a dlistint_t linked list
+ * @head: pointer to the pointer to the 1st node in the list
+ * @index: index of node to delete
  *
- * Return: size_t (number of nodes)
+ * Return: 1 if it succeeded, -1 if it failed
  */
-
-size_t dlistint_len(const dlistint_t *h)
-{
-	size_t total_nodes = 0;
-	int i;
-
-	for (i = 0; h != NULL; i++)
-	{
-		h = h->next;
-		total_nodes++;
-	}
-	return (total_nodes);
-}
-
-/**
- * delete_dnodeint_at_index - deletes a node at an index
- * @head: head of dlist
- * @index: index at which node is to be removed
- *
- * Return: 1 if success, -1 if fail
- */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp, *bef, *aft;
-	unsigned int len, i;
+	dlistint_t *trav, *tmp;
 
-	if (!*head)
+	if (head == NULL || *head == NULL)
 		return (-1);
-	if (*head)
-	{
-		while ((*head)->prev)
-			(*head) = (*head)->prev;
-	}
-	len = dlistint_len(*head);
+
+	/* delete 1st node */
 	if (index == 0)
 	{
-		temp = *head;
+		tmp = *head;
 		*head = (*head)->next;
-		free(temp);
-	}
-	else if (index > 0 && index < len - 1)
-	{
-		bef = *head;
-		for (i = 0; i < index - 1; i++)
-			bef = bef->next;
-		temp = bef->next;
-		aft = temp->next;
-		bef->next = aft;
-		free(temp);
-	}
-	else if (index == len - 1)
-	{
-		bef = *head;
-		temp = (*head)->next;
-		while (temp->next  != NULL)
-			bef = bef->next;
-		bef->next = NULL;
-		free(temp);
-	}
-	else
-		return (-1);
+		if (*head)
+			(*head)->prev = NULL;
 
-	return (1);
+		free(tmp);
+		return (1);
+	}
+
+	trav = *head;
+	while (trav && index--)
+		trav = trav->next;
+
+	if (trav)
+	{
+		trav->prev->next = trav->next;
+		if (trav->next)
+			trav->next->prev = trav->prev;
+
+		free(trav);
+		return (1);
+	}
+
+	return (-1);
 }

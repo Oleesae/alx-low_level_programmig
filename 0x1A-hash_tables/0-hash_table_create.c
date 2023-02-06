2cf333011edcd2c77dd6ed00c4ffaf027e56b982
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-06 15:22:50 +0100

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table == NULL)
 		return (NULL);
 
+	new_table->array = malloc(sizeof(void *) * size);
+
+	if (!new_table)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t) * size);
 	return (new_table);
 }

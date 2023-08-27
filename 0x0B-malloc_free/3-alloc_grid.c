23317845ee25d3551255f828bf33ad4b7409abdd
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-08-27 21:13:03 +0100

    alloc for large array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,22 +13,21 @@ int **alloc_grid(int width, int height)
 	int i;
 	int j;
 
-	int **grid = malloc(sizeof(int *) * height);
+	int** grid = (int**)malloc(height * sizeof(int*));
 
 	if (width < 1 || height < 1)
-		return (NULL);
-
-	if (!grid)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
-		grid[i] = malloc(sizeof(int) * 3);
-
-	for (i = 0; i < height; i++)
 	{
+		free(grid);
+		return (NULL);
+	}
+
+
+	for (i = 0; i < height; i++)
+		grid[i] = (int *)malloc(sizeof(int) * width);
+
+	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
-	}
 
 	return (grid);
 }

f16d1164c7ec6ef2401a5c07ff742dfe9b9fd23e
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-10-07 01:04:09 +0100

    check create table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ *
+ * Return: NULL if fail, pointer of newly created hash table on success
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
+	{
+		return (NULL);
+	}
+
+	new_table->size = size;
+	new_table->array = malloc(size * sizeof(hash_node_t));
+	if (!new_table->array)
+	{
+		return(NULL);
+	}
+
+	return (new_table);
+}

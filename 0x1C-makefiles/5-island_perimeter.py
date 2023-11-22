3ee2209cbe121ab03b1454ba76b36fcb718d01d6
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-11-22 19:47:02 +0100

    add: island_perimeter check

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,23 +9,21 @@ def island_perimeter(grid):
     grid: a list of list of integers, is rectangular, width
     and height don't exceed"""
 
-    def island_perimeter(grid):
-        perimeter = 0
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == 1:
-                    perimeter += 4  # Initially assume all sides are a perimeter
-                    # Check each side if it's not a perimeter
-                    if row - 1 >= 0 and grid[row - 1][col] == 1:
-                        perimeter -= 1
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                perimeter += 4  # Initially assume all sides are a perimeter
+                # Check each side if it's not a perimeter
+                if row - 1 >= 0 and grid[row - 1][col] == 1:
+                    perimeter -= 1
 
-                    if col - 1 >= 0 and grid[row][col - 1] == 1:
-                        perimeter -= 1
+                if col - 1 >= 0 and grid[row][col - 1] == 1:
+                    perimeter -= 1
 
-                    if row + 1 < len(grid) and grid[row + 1][col] == 1:
-                        perimeter -= 1
+                if row + 1 < len(grid) and grid[row + 1][col] == 1:
+                    perimeter -= 1
 
-                    if col + 1 < len(grid[0]) and grid[row][col + 1] == 1:
-                        perimeter -= 1
-
-                return perimeter
+                if col + 1 < len(grid[row]) and grid[row][col + 1] == 1:
+                    perimeter -= 1
+    return perimeter

0e2931311548b4a0c79e214ca6fd722d82c653f0
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-01-31 23:08:48 +0100

    add:free

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,5 +5,11 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
+int _isspace(char c);
+char *skipspace(char *str);
+unsigned int countchars(char *str);
+unsigned int countwords(char *str);
+char **strtow(char *str);
 
 #endif /* MAIN_H */
